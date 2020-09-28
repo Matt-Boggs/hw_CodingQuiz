40bb04d37ff857406887573c6c3fff852abebc27
Matt Boggs <matt.david.boggs@gmail.com>
2020-09-28 15:43:28 -0600

    added notes from class

--- javascript.js
+++ javascript.js
@@ -2,15 +2,16 @@
 // WHEN I click the start button
 
 
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
+// THEN a timer starts 
+//THEN I am presented with a question
+// WHEN I answer a question(how does code know if its a correct answer)
+// THEN I am presented with another question <----- "another" i.e. Function
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
 // WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
+// THEN the game is over  (another function)
 // WHEN the game is over
-// THEN I can save my initials and score
+// THEN I can save my initials and score (will learn how to do this 929)
 
 
-//custom attribute for which answer is the correct one
+//custom attribute for which answer is the correct one (attribute is data-correct)
